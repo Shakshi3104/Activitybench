85ab311e70f9e906c698616a07cf506dc12e4b4a
Shakshi3104 <watchosastrom3104@gmail.com>
2021-05-17 10:34:09 +0900

    Initial Commit

---
+++ Activitybench 2/Activitybench_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Activitybench_2App.swift
+//  Activitybench 2
+//
+//  Created by MacBook Pro M1 on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct Activitybench_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Activitybench 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Activitybench 2
+//
+//  Created by MacBook Pro M1 on 2021/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
