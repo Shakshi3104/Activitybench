5fd12aaa7c3edc7ebfd2e0496367ae6c4f9407da
Shakshi3104 <watchosastrom3104@gmail.com>
2021-08-26 11:58:52 +0900

    Replace dataset and improved performance of loading dataset

--- Activitybench 2/Model/Benchmark/BenchmarkDataset.swift
+++ Activitybench 2/Model/Benchmark/BenchmarkDataset.swift
@@ -8,42 +8,49 @@
 import Foundation
 import CoreML
 
+
+private func loadData() -> [[Double]] {
+    guard let fileURL: URL = Bundle.main.url(forResource: "activitybench_xtest_v2", withExtension: "csv") else {
+                fatalError("Couldn't find activitybench_xtest_v2.csv")
+    }
+
+    guard let csvString = try? String(contentsOf: fileURL, encoding: String.Encoding.utf8) else {
+        fatalError("Couldn't read activitybench_xtest_v2.csv")
+    }
+
+    // 改行コード区切り->カンマ区切り->Doubleに変換-> [[Double]]
+    let outData = csvString.components(separatedBy: .newlines).map { $0.components(separatedBy: ",").map { Double($0) ?? Double.nan } }
+
+    return outData
+}
+
+private func loadDataAsMLMultiArray() -> [MLMultiArray] {
+    let dataDouble = loadData()
+    let dataMLMultiArray = dataDouble.map { try! MLMultiArray.fromDouble($0) }
+    return dataMLMultiArray
+}
+
+private func loadTarget() -> [Int] {
+    guard let fileURL = Bundle.main.url(forResource: "activitybench_ytest_v2", withExtension: ".csv") else {
+        fatalError("Couldn't find activitybench_ytest_v2.csv")
+    }
+    
+    guard let csvString = try? String(contentsOf: fileURL, encoding: String.Encoding.utf8) else {
+        fatalError("Couldn't read activitybench_ytest_v2.csv")
+    }
+    
+    // 改行コード区切り->Intに変換-> [Int]
+    let outTarget = csvString.components(separatedBy: .newlines).map { Int($0) ?? -1 }
+    return outTarget
+}
+
+
 internal struct BenchmarkDataset {
-    var data: [MLMultiArray] { return loadDataAsMLMultiArray() }
-    var target: [Int] { return loadTarget() }
+    let data: [MLMultiArray]
+    let target: [Int]
     
-    private func loadData() -> [[Double]] {
-        guard let fileURL: URL = Bundle.main.url(forResource: "activitybench_xtest", withExtension: "csv") else {
-                    fatalError("Couldn't find activitybench_xtest.csv")
-        }
-
-        guard let csvString = try? String(contentsOf: fileURL, encoding: String.Encoding.utf8) else {
-            fatalError("Couldn't read activitybench_xtest.csv")
-        }
-
-        // 改行コード区切り->カンマ区切り->Doubleに変換-> [[Double]]
-        let outData = csvString.components(separatedBy: .newlines).map { $0.components(separatedBy: ",").map { Double($0) ?? Double.nan } }
-
-        return outData
-    }
-    
-    private func loadDataAsMLMultiArray() -> [MLMultiArray] {
-        let dataDouble = loadData()
-        let dataMLMultiArray = dataDouble.map { try! MLMultiArray.fromDouble($0) }
-        return dataMLMultiArray
-    }
-    
-    private func loadTarget() -> [Int] {
-        guard let fileURL = Bundle.main.url(forResource: "activitybench_ytest", withExtension: ".csv") else {
-            fatalError("Couldn't find activitybench_ytest.csv")
-        }
-        
-        guard let csvString = try? String(contentsOf: fileURL, encoding: String.Encoding.utf8) else {
-            fatalError("Couldn't read activitybench_ytest.csv")
-        }
-        
-        // 改行コード区切り->Intに変換-> [Int]
-        let outTarget = csvString.components(separatedBy: .newlines).map { Int($0) ?? -1 }
-        return outTarget
+    init() {
+        data = loadDataAsMLMultiArray()
+        target = loadTarget()
     }
 }
